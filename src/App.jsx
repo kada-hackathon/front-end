5cbc3b2143fae55fe3fbb78b18dc4596e52adf7a
jovan pilot <jovanthesar06@gmail.com>
2025-10-28 14:58:35 +0700

    Editor Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import PostPage from './pages/post/post';
+import Login from './components/Login/Login.jsx';
+import Editor from '@/components/TiptapEditor/Editor'; 
+import './index.css';
+import MenuBar from './components/menubar/menubar';
+import Navbar from './components/navbar/navbar';
+import FriendsList from './components/friends-list/friend-list';
 
 function App() {
- return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<PostPage />} />
-      </Routes>
-    </Router>
-  )
+    return (
+      <>
+      <MenuBar />
+      <Navbar />
+      <Editor />
+      </>
+  );
 }
 
-export default App
+export default App;
